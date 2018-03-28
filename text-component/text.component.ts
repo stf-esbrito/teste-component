0a638bbc2bd57c86150fa5999a3de728f43b9e17
Eduardo Santos de Brito <esbrito@latam.stefanini.com>
2018-03-28 13:10:47 -0300

    commit inicial

---
+++ text-component/text.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'text-component',
+    templateUrl: './text-component.html',
+    styleUrls: ['./text-component.css']
+  })
+
+  export class TextComponent{
+
+  }
