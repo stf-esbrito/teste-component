98ca2bd09e8fb9f456bb086a5a998cd4c6e449d7
Eduardo Santos de Brito <esbrito@latam.stefanini.com>
2018-03-28 13:18:51 -0300

    index e package

---
+++ text.module.ts
@@ -0,0 +1,12 @@
+import { TextComponent } from "./text-component/text.component";
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [
+        TextComponent
+    ],
+    exports: [
+        TextComponent
+    ]
+  })
+  export class TextModule { }
